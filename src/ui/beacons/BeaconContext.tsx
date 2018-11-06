3a30f3623d8ec21ee2cc6a9486b6957376fdcfb9
suprko <lucas@peerio.com>
2018-11-06 16:27:35 -0500

    chore: remove testing 'activeBeacon' functions

--- src/ui/beacons/BeaconContext.tsx
+++ src/ui/beacons/BeaconContext.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { action, observable } from 'mobx';
 import { observer, Provider } from 'mobx-react';
 
-import { BeaconActionsProps } from './types';
-
 @observer
 export default class BeaconWrapper extends React.Component<{}> {
     // Store all possible beacons in here (passed from <Beacon> components)
@@ -16,27 +14,9 @@ export default class BeaconWrapper extends React.Component<{}> {
         }
     }
 
-    // Currently active beacon(s)
-    @observable activeBeacon: string;
-
-    @action.bound
-    activateBeacon(option: string): void {
-        this.activeBeacon = option;
-    }
-
-    beaconActions: BeaconActionsProps = {
-        activeBeacon: this.activeBeacon,
-        activateBeacon: this.activateBeacon,
-        initializeBeacon: this.initializeBeacon
-    };
-
     render() {
         return (
-            <Provider
-                beaconActions={this.beaconActions}
-                beaconStore={this.beaconStore}
-                beaconInit={this.initializeBeacon}
-            >
+            <Provider beaconStore={this.beaconStore} beaconInit={this.initializeBeacon}>
                 <>
                     {this.props.children}
                     {/* <BeaconItself /> */}
